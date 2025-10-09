7fdd5e9b6d55f0cb0555c33c14dadcbf8e76c3d9
m2dyt <roman16050811@mail.ru>
2025-10-09 11:33:46 +0300

    .

---
+++ comp_graf_1/LAB_5/LAB_5.cpp
@@ -0,0 +1,302 @@
+﻿
+#include <GL/glut.h>
+#include<Windows.h>
+#include <math.h>
+using namespace std;
+
+#define checkImageWidth 192
+#define checkImageHeight 192
+GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+GLuint texName;
+
+GLuint myList;
+bool fogFlag = 0;
+// задание нурб-поверхности
+GLfloat ctrlPoints[4][4][3] = {
+    {{-3.5, 1, -4.5}, {-0.5, 1.0, -4.5}, {5.0, 1.0, -4.5}, {3.5, 1.0, -4.5}},
+    {{-3.5, 1, -0.5}, {-0.5, -2.0, -0.5}, {5.5, -2.0, -5}, {3.5, 1.0, -0.5}},
+    {{-3.5, 1, 0.5}, {-1.5, 2.0, 3.5}, {5.5, 1.0, 3.5},{3.5, 1.0, 3.5} },
+    {{-3.5, 1, 4.5}, {-0.5, 2.0, 4.5}, {5.5, 1.0, 4.5}, {3.5, 1.0,4.5}}
+};
+
+
+void figure() {
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glBindTexture(GL_TEXTURE_2D, texName);
+
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_POLYGON);
+    glVertex3f(-1.0, 0.0, 0.0); glTexCoord2f(0, 0);
+    glVertex3f(0.0, 0.0, 1.0);  glTexCoord2f(0, 1);
+    glVertex3f(0.0, 1.0, 0.0);  glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(0.0, 0.0, 1.0); glTexCoord2f(0, 0);
+    glVertex3f(1.0, 0.0, 0.0); glTexCoord2f(0, 1);
+    glVertex3f(0.0, 1.0, 0.0); glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-1.0, 0.0, 0.0); glTexCoord2f(0, 0);
+    glVertex3f(0.0, -1.0, 0.0); glTexCoord2f(0, 1);
+    glVertex3f(0.0, 0.0, 1.0);  glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(0.0, 0.0, 1.0);  glTexCoord2f(0, 0);
+    glVertex3f(0.0, -1.0, 0.0); glTexCoord2f(0, 1);
+    glVertex3f(1.0, 0.0, 0.0);  glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-1.0, 0.0, 0.0); glTexCoord2f(0, 0);
+    glVertex3f(0.0, 0.0, -1.0); glTexCoord2f(0, 1);
+    glVertex3f(0.0, 1.0, 0.0);  glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-1.0, 0.0, 0.0); glTexCoord2f(0, 0);
+    glVertex3f(0.0, -1.0, 0.0); glTexCoord2f(0, 1);
+    glVertex3f(0.0, 0.0, -1.0); glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(0.0, 0.0, -1.0); glTexCoord2f(0, 0);
+    glVertex3f(0.0, -1.0, 0.0); glTexCoord2f(0, 1);
+    glVertex3f(1.0, 0.0, 0.0);  glTexCoord2f(0, 0);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+
+    //сохраняет как список
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_POLYGON);
+    glVertex3f(2, 0, 0); glTexCoord2f(0, 1);
+    glVertex3f(2.5, 0.0, 0.5);  glTexCoord2f(0, 0);
+    glVertex3f(2.5, 0.5, 0);  glTexCoord2f(0, 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(2.5, 0, 0.5); glTexCoord2f(0, 1);
+    glVertex3f(3, 0, 0); glTexCoord2f(0, 0);
+    glVertex3f(2.5, 0.5, 0); glTexCoord2f(0, 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(2, 0, 0); glTexCoord2f(0, 1);
+    glVertex3f(2.5, -0.5, 0); glTexCoord2f(0, 0);
+    glVertex3f(2.5, 0, 0.5);  glTexCoord2f(0, 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(2.5, 0, 0.5);  glTexCoord2f(0, 1);
+    glVertex3f(2.5, -0.5, 0); glTexCoord2f(0, 0);
+    glVertex3f(3, 0, 0);  glTexCoord2f(0, 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(2, 0, 0); glTexCoord2f(0, 0);
+    glVertex3f(2.5, 0, -0.5); glTexCoord2f(0, 1);
+    glVertex3f(2.5, 0.5, 0);  glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(2, 0, 0); glTexCoord2f(0, 0);
+    glVertex3f(2.5, -0.5, 0); glTexCoord2f(0, 1);
+    glVertex3f(2.5, 0, -0.5); glTexCoord2f(0, 0);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex3f(2.5, 0, -0.5); glTexCoord2f(0, 0);
+    glVertex3f(2.5, -0.5, 0); glTexCoord2f(0, 1);
+    glVertex3f(3, 0, 0);  glTexCoord2f(0, 0);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+
+}
+
+//задается поверхность
+void surface() {
+    int i, j;
+    glPushMatrix();
+    glTranslatef(0, -2, 0);
+    glColor3f(1, 0, 0);
+    for (int j = 0; j <= 30; j++) {
+        glBegin(GL_LINE_STRIP);
+        for (i = 0; i <= 100; i++)
+            glEvalCoord2f((GLfloat)i / 100.0, (GLfloat)j / 30.0);
+        glEnd();
+        glBegin(GL_LINE_STRIP);
+        for (i = 0; i <= 100; i++)
+            glEvalCoord2f((GLfloat)j / 30.0, (GLfloat)i / 100.0);
+        glEnd();
+    }
+    glPopMatrix();
+    //glFlush();
+}
+
+void makeCheckImage() {
+    int i, j, c;
+    for (i = 0; i < checkImageHeight; i++)
+        for (j = 0; j < checkImageWidth; j++) {
+            c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 245;
+            checkImage[i][j][0] = (GLubyte)c;
+            checkImage[i][j][1] = (GLubyte)c;
+            checkImage[i][j][2] = (GLubyte)c;
+            checkImage[i][j][3] = (GLubyte)255;
+        }
+}
+
+void init() {
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_MAP2_VERTEX_3);
+    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+    glClearColor(0, 0, 0, 0.0);
+    glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &ctrlPoints[0][0][0]);
+    glMapGrid2f(20, 0, 1, 20, 0, 1);
+    glShadeModel(GL_FLAT);
+
+    makeCheckImage();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    glGenTextures(1, &texName);
+    glBindTexture(GL_TEXTURE_2D, texName);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
+
+    myList = glGenLists(1);
+    glNewList(myList, GL_COMPILE);
+    figure();
+    glEndList();
+}
+
+void light_0() {
+    float light0_ambient[] = { -1.0, 0.4, 0.5, 1.0 }; //цвет фонового излучения источника света
+    float light0_diffuse[] = { -0.5, 0.5, -0.5 }; // устанавливаем диффузный цвет света
+    float light0_direction[] = { -3, -4, -2, 1.0 }; // устанавливаем направление света
+    glEnable(GL_LIGHT0); // разрешаем использовать light0
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse); // устанавливаем источнику света light0 диффузный свет, который указали ранее
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_direction); // устанавливаем направление источника света, указанное ранее
+}
+void light_1() {
+    float light1_ambient[] = { -0.4, -1.0, 0.0, 1.0 };
+    float light1_diffuse[] = { -0.4, -0.4, 1.0 };
+    float light1_direction[] = { 5.0, -4, 7, 1.0 };
+    glEnable(GL_LIGHT1);
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light1_ambient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_direction);
+}
+void light_2() {
+    float light2_ambient[] = { 0.0, 0.0, 1.0, 1.0 };
+    float light2_diffuse[] = { 0.0, 0.0, 1.0 };
+    float light2_direction[] = { -5.0, -4, 3, 1.0 };
+    glEnable(GL_LIGHT2);
+    glLightfv(GL_LIGHT2, GL_AMBIENT, light2_ambient);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_diffuse);
+    glLightfv(GL_LIGHT2, GL_POSITION, light2_direction);
+}
+
+void fog() {
+    glEnable(GL_FOG);
+    GLfloat fogColor[4] = { 0.5,0.5,0.5,1 };
+    GLint fogMode = GL_EXP;
+    glFogi(GL_FOG_MODE, fogMode);
+    glFogfv(GL_FOG_COLOR, fogColor);
+    glFogf(GL_FOG_DENSITY, 0.2);
+    glHint(GL_FOG_HINT, GL_NICEST);
+    glFogf(GL_FOG_START, 1);
+    glFogf(GL_FOG_END, 5);
+
+}
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'x':
+        glRotatef(30., 1.0, 0.0, 0.0);
+        glutPostRedisplay();
+        break;
+    case 'y':
+        glRotatef(30., 0.0, 1.0, 0.0);
+        glutPostRedisplay();
+        break;
+    case 'z':
+        glRotatef(30., 0.0, 0.0, 1.0);
+        glutPostRedisplay();
+        break;
+    case 'i':
+        glLoadIdentity();
+        gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+        glutPostRedisplay();
+        break;
+    case 'f':
+        fogFlag = (fogFlag + 1) % 2;
+        glutPostRedisplay();
+        break;
+    }
+}
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    light_0();
+    light_1();
+    light_2();
+
+    if (fogFlag) fog();
+    else glDisable(GL_FOG);
+    surface();
+
+    glPushMatrix();
+    glScalef(0.8, 0.8, 0.8);
+    glTranslatef(-2, 0, 0);
+    glCallList(myList);
+    glPopMatrix();
+    glPushMatrix();
+    glScalef(0.5, 0.5, 0.5);
+    glTranslatef(3.5, 0, 0);
+    glCallList(myList);
+    glPopMatrix();
+    glFlush();
+    glutSwapBuffers();
+    Sleep(100);
+}
+
+
+void reshape(int w, int h) {
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(30, (GLfloat)w / (GLfloat)h, 1, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+}
+
+int main(int argc, char** argv)
+{
+    //инициализация библиотеки
+    glutInit(&argc, argv);
+
+    //установка параметров окна
+    //glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);//  Запрашиваем окно с поддержкой двойной буферизации, z-буферизации и цветовой схемы True Color
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(350, 150);
+
+    glutCreateWindow("лабораторная работа");
+    init();
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+
+    return 0;
+}
+
--- comp_graf_1/OpenGL/OpenGL.cpp
+++ comp_graf_1/OpenGL/OpenGL.cpp
@@ -1,146 +1,233 @@
-﻿#include <GL/glut.h>
-#include <stdarg.h>
-#include <cmath>
-#define GL_GLEXT_PROTOTYPES
+﻿#include <iostream>
+#include <GL/glut.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
 
-void init(void) {
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_SMOOTH);
+// параметры материалов куба
+float cubeDif[] = { 0.3, 0.5, 0.2 };
+float cubeAmb[] = { 0.5, 0.5, 0.5 };
+float cubeSpec[] = { 0.8, 0.7, 0.8 };
+float cubeShininess = 40;
 
-	static float lmodel_twoside[] = { GL_TRUE };
-	static float lmodel_oneside[] = { GL_FALSE };
+float cubeDif1[] = { 0.0f , 0.9f , 0.8f };
+float cubeAmb1[] = { 0.2f , 0.2f , 0.2f };
+float cubeSpec1[] = { 0.9f , 0.9f , 0.9f };
+float cubeShininess1 = 0.7f * 128;
 
-	static float ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-	static float diffuse[] = { 1.0, 0.0, 0.0, 1.0 };
-	static float position[] = { -2.0, 2.0, 2.0, 0.0 };
+// параметры материалов сферы
+float sphereDif[] = { 1.0f , 0.9f , 0.0f };
+float sphereAmb[] = { 0.6f , 0.0f , 0.8f };
+float sphereSpec[] = { 0.2f , 0.2f , 0.8f };
+float sphereShininess = 0.3f * 128;
 
-	static float ambient1[] = { 0.3, 0.3, 0.3, 1.0 };
-	static float diffuse1[] = { 0.0, 1.0, 0.0, 1.0 };
-	static float position1[] = { 2.0, 2.0, 0.0, 0.0 };
+// переменные необходимая для вращения
+int angleX = 0, angleY = 0, angleZ = 0;
+int q = 0, a = 0, z = 0;
+float k = 1;
 
-	static float ambient2[] = { 1.0, 1.0, 1.0, 1.0 };
-	static float diffuse2[] = { 0.0, 0.0, 1.0, 1.0 };
-	static float position2[] = { 0.0, -2.0, 0.0, 0.0 };
+//Инициализация параметров и источника света
+void init(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
+	glPushMatrix();
+	glRotatef(q, 0.7, 0.0, 0.0);
+	glRotatef(q, 0.0, 0.6, 0.0);
+	glRotatef(q, 0.0, 0.0, 0.8);
 
-	static float frontMatShininess[] = { 60.0 };//степень в зерк отраж
-	static float frontMatSpecular[] = { 0.5, 0.5, 0.2, 1.0 };
-	static float frontMatDiffuse[] = { 0.5, 0.5, 0.28, 1.0 };
-	static float backMatShininess[] = { 60.0 };
-	static float backMatSpecular[] = { 0.5, 0.5, 0.2, 1.0 };
-	static float backMatDiffuse[] = { 1.0, 0.9, 0.2, 1.0 };
+	GLfloat lightAmbient[] = { 0.7 , 0.0 , 0.0 , 1.0 };
+	GLfloat lightDiff[] = { 1.0 , 0.3 , 0.3 , 1.0 };
+	GLfloat lightSpec[] = { 0.3 , 0.3 , 0.3 , 1.0 };
+	GLfloat lightPosit[] = { 1.0 , 1.0 , 2.0 , 0.0 };
 
-	static float lmodelAmbient[] = { 1.0, 1.0, 1.0, 1.0 };
+	// устанавливаем параметры источника света
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);//цвет фонового излучения источника света
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiff);//цвет рассеянного излучения источника света
+	glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpec);//цвет зеркального излучения источника света
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosit);//Первые три компоненты (x, y, z) задают вектор направления, а w = 0 (иначе источник превратится в точечный).
+	glEnable(GL_LIGHT0);
 
+	glPopMatrix();
+	glPushMatrix();
+
+	//текущую матрицу на матрицу вращения
+	glRotatef(a, 0.7, 0.0, 0.0);
+	glRotatef(a, 0.0, 0.6, 0.0);
+	glRotatef(a, 0.0, 0.0, 0.8);
+
+	GLfloat lightAmbient1[] = { 0.1 , 0.8 , 0.9 , 1.1 };
+	GLfloat lightDiff1[] = { 0.5 , 0.2 , 0.6 , 1.0 };
+	GLfloat lightSpec1[] = { 0.1 , 0.1 , 0.1 , 1.0 };
+	GLfloat lightPosit1[] = { -1.0 , 1.0 , 2.0 , 0.0 };
+
+	// устанавливаем параметры источника света
+	glLightfv(GL_LIGHT1, GL_AMBIENT, lightAmbient1);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDiff1);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, lightSpec1);
+	glLightfv(GL_LIGHT1, GL_POSITION, lightPosit1);
+	glEnable(GL_LIGHT1);
+
+	glPopMatrix();
+	glPushMatrix();
+
+	glRotatef(z, 0.7, 0.0, 0.0);
+	glRotatef(z, 0.0, 0.6, 0.0);
+	glRotatef(z, 0.0, 0.0, 0.8);
+
+	GLfloat lightAmbient2[] = { 0.9 , 0.2 , 0.0 , 1.0 };
+	GLfloat lightDiffuse2[] = { 0.1 , 0.7 , 0.3 , 1.0 };
+	GLfloat lightSpecular2[] = { 0.1 , 0.1 , 0.1 , 1.0 };
+	GLfloat lightPosition2[] = { 0.0 , -1.0 , 2.0 , 0.0 };
+
+	// устанавливаем параметры источника света
+	glLightfv(GL_LIGHT2, GL_AMBIENT, lightAmbient2);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, lightDiffuse2);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, lightSpecular2);
+	glLightfv(GL_LIGHT2, GL_POSITION, lightPosition2);
+	glEnable(GL_LIGHT2);
+
+	glPopMatrix();
+	glEnable(GL_LIGHTING);//включаем освещение и источник света
 	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-
-	glLightfv(GL_LIGHT1, GL_AMBIENT, ambient); //фоновое изл источ
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse);//цвет рассеяного ист
-	glLightfv(GL_LIGHT1, GL_POSITION, position);//направленный свет
-
-	glLightfv(GL_LIGHT2, GL_AMBIENT, ambient1);
-	glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuse1);
-	glLightfv(GL_LIGHT2, GL_POSITION, position1);
-
-	glLightfv(GL_LIGHT3, GL_AMBIENT, ambient2);
-	glLightfv(GL_LIGHT3, GL_DIFFUSE, diffuse2);
-	glLightfv(GL_LIGHT3, GL_POSITION, position2);
-
-	//материал
-	glMaterialfv(GL_FRONT, GL_SHININESS, frontMatShininess);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, frontMatSpecular); //цвет зеркального отр
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, frontMatDiffuse);
-	glMaterialfv(GL_BACK, GL_SHININESS, backMatShininess);
-	glMaterialfv(GL_BACK, GL_SPECULAR, backMatSpecular);
-	glMaterialfv(GL_BACK, GL_DIFFUSE, backMatDiffuse);
-
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodelAmbient);
-	glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_twoside);
-
+}
+void reshape(int w, int h) {
+	glViewport(0, 0, w, h);//устанавливаем размер области вывода равным размеру окна
+	glMatrixMode(GL_PROJECTION);//задаем матрицу проекции с учетом размеров окна
+	glLoadIdentity();//очистки текущей матрицы для будущих преобразований
+	gluPerspective(120.0, w / h, 1, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 1.0, 5, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0f);
+	glFlush();
 }
 
 void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glDisable(GL_LIGHTING);
-	glDisable(GL_LIGHT1);
-	glDisable(GL_LIGHT2);
-	glDisable(GL_LIGHT3);
-
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT1);
-	glEnable(GL_LIGHT2);
-	glEnable(GL_LIGHT3);
-	glEnable(GL_DEPTH_TEST);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//очистка буфера кадра и буфера глубины
+	glPushMatrix();
+	glMaterialfv(GL_FRONT, GL_AMBIENT, cubeAmb);//фоновый цвет материала
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, cubeDif);//диффузный цвет материала
+	glMaterialfv(GL_FRONT, GL_SPECULAR, cubeSpec);//зеркальный цвет материала
+	glMaterialf(GL_FRONT, GL_SHININESS, cubeShininess);//показатель зеркального отражения
 
 	glPushMatrix();
-	glTranslatef(1.0f, 0.1f, 0.1f); //перенос координат
-	glutSolidSphere(0.2, 50, 40); //шарик
+	glTranslatef(-4.75, 0.0, -1.0);
+	glRotatef(angleX, 0.7, 0.0, 0.0);
+	glRotatef(angleY, 0.0, 0.6, 0.0);
+	glRotatef(angleZ, 0.0, 0.0, 0.8);
+	glutSolidCube(2.5);
 	glPopMatrix();
 
+	glMaterialfv(GL_FRONT, GL_AMBIENT, cubeAmb1);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, cubeDif1);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, cubeSpec1);
+	glMaterialf(GL_FRONT, GL_SHININESS, cubeShininess1);
 	glPushMatrix();
-	glTranslatef(0.75f, -0.45f, 0.4f);
-	glColor3d(1, 0, 1);
-	glutSolidSphere(0.2, 50, 40);
+	glTranslatef(0.0, -3.0, 0.0);
+	glRotatef(angleX, 0.7, 0.0, 0.0);
+	glRotatef(angleY, 0.0, 0.6, 0.0);
+	glRotatef(angleZ, 0.0, 0.0, 0.8);
+	glutSolidCube(1.0);
 	glPopMatrix();
 
+	// сфера
+	glEnable(GL_BLEND); //включаем прозрачность
+	glBlendFunc(GL_ONE, GL_ONE);
+	glDepthMask(GL_FALSE);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, sphereAmb);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, sphereDif);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, sphereSpec);
+	glMaterialf(GL_FRONT, GL_SHININESS, sphereShininess);
 	glPushMatrix();
-	glTranslatef(0.0f, -1.0f, 0.7f);
-	glutSolidCube(0.5);
+
+	glRotatef(15, 0.7, 0.0, 0.0);
+	glRotatef(angleX, 0.7, 0.0, 0.0);
+	glRotatef(angleY, 0.0, 0.6, 0.0);
+	glRotatef(angleZ, 0.0, 0.0, 0.8);
+	glTranslatef(3.75, 0.0, -1.0);
+
+	glScalef(k, k, k); //изменение масштаба
+	glutSolidSphere(2, 150, 150);
 	glPopMatrix();
+	glDepthMask(GL_TRUE);
+	glDisable(GL_BLEND);
 
-
+	
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_ONE, GL_ONE);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, cubeAmb1);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, cubeDif1);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, cubeSpec1);
+	glMaterialf(GL_FRONT, GL_SHININESS, cubeShininess1);
 	glPushMatrix();
-	glTranslatef(0.6f, 1.0f, -0.4f);
-	glutSolidCube(0.5);
-	glPopMatrix();
+	glTranslatef(0.0, 0.0, 0.0);
+	glRotatef(angleX, 0.7, 0.0, 0.0);
+	glRotatef(angleY, 0.0, 0.6, 0.0);
+	glRotatef(angleZ, 0.0, 0.0, 0.8);
+	glutSolidCube(1);
+	glDisable(GL_BLEND);
+	glPopMatrix();// вид камеры сд другой  стороны
 
-	glutSwapBuffers();
-	glutPostRedisplay();
+
+	init();//заново запускаем эту функцию чтобы сменить координаты камеры
+	glFlush();//выводим сцену на экран
+}
+// выход при нажатии esc
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	case 'q':
+		angleX = (angleX + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'w':
+		angleY = (angleY + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'e':
+		angleZ = (angleZ + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'r':
+		q = (q - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 't':
+		a = (a - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'y':
+		z = (z - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'u':
+		z = (z + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'i':
+		k += 0.1;
+		if (k >= 2)
+			k = -2;
+		glutPostRedisplay();
+		break;
+	default:
+		break;
+	}
 }
 
-
-void keyboard1(int key, int x, int y) {
-	if (key == GLUT_KEY_RIGHT)
-		glRotatef(-2, 1, 0, 0);
-	else if (key == GLUT_KEY_LEFT)
-		glRotatef(2, 1, 0, 0);
-	else if (key == GLUT_KEY_UP)
-		glRotatef(-2, 0, 1, 0);
-	else if (key == GLUT_KEY_DOWN)
-		glRotatef(2, 0, 1, 0);
-
-	glutPostRedisplay();
-}
-
-
-
-void reshape(int w, int h) {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60.0, (double)w / (double)h, 1.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(
-		2.0f, 0.0f, 2.0f,
-		0.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f
-	);
-}
-
-
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Работа 3");
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("Рандом фигурки");
 	init();
-	glutSpecialFunc(keyboard1);
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+
 	glutMainLoop();
 	return 0;
 }
